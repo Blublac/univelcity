b57f015d6bcfe778ee21b9071a4fbad8042a0d56
Blublac <kenechukwuc7@gmail.com>
2021-09-09 15:19:55 +0100

    initial commit

---
+++ first class.py
@@ -0,0 +1,47 @@
+#welcome = 5
+
+#my_int = 40
+#my_float = 50.45
+#my_string = "hello world"
+#my_bool = true
+#type_of_data = type(my_string)
+#print (type_of_data)
+
+#print(welcome)
+#my_list = ["a boy ",45, true, 45 []]
+#my_tuple = ()
+#my_set = {2,3,6,6}
+a = 3
+b = 6
+c = 3
+numerator = -b+(b**2 - 4*a*c)**0.5
+denominator = 2*a
+w = numerator/denominator
+print(w)
+y = 5
+z = 2
+q = 15
+r = 4
+nume = (y**0.5) + z
+deno = (q**(r**0.5))
+d = nume/deno
+print(d)
+p = 10000
+r = 0.08
+n = 12
+t = 5
+a = p*(1+(r/n))**(n*t)
+print (a)
+a = 4; b = 9; c = 8;
+s = a+b+c
+area = (s*(s-a)*(s-b)*(s-c))**0.5
+print(area)
+father_age = 47
+father_age +=10
+daughter = father_age//2
+print(daughter)
+salary = 100000
+savings = 0.1
+amount_saved = salary*savings
+anual_savings = amount_saved*12
+print(anual_savings)
