78c60b6c7597f5e2613ca26647df02f84fc950b9
Blublac <kenechukwuc7@gmail.com>
2021-09-10 19:37:37 +0100

    first commit for assignment 1

---
+++ assignment.py
@@ -0,0 +1,9 @@
+#write a program thar=t converts 200 naira to dollar. assume $30 is #1
+USD = 30
+AMOUNT = 200
+NGN = AMOUNT * USD
+print(NGN)
+#Given that g(x)=2x**3-4x solve for g(7)
+X = 7
+G = 2*(X**3)-(4*X)
+print(G)
